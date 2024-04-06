9d8ec3ba6dded226321d5690667cbc035f608de7
Vladislav <bigmag174@yandex.ru>
2024-04-06 18:44:49 +0500

    Добавил навигацию, viewBinding и сверстал экран поиска

---
+++ app/src/main/java/com/example/cinemaaggregator/RootActivity.kt
@@ -0,0 +1,15 @@
+package com.example.cinemaaggregator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cinemaaggregator.databinding.ActivityRootBinding
+
+class RootActivity : AppCompatActivity() {
+
+    private val binding: ActivityRootBinding by lazy { ActivityRootBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/example/cinemaaggregator/common/search/presentation/fragment/SearchFragment.kt
@@ -0,0 +1,19 @@
+package com.example.cinemaaggregator.common.search.presentation.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.cinemaaggregator.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
